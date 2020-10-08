922fbbccbcc7b4ff5098bd68b8d6e6df346f052b
vasquez_d <vasquezd@oregonstate.edu>
2020-10-08 15:53:06 -0700

    Starting Site Redesign

---
+++ template/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<!DOCTYPE html>
+<html>
+    <head>
+		<!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="" />
+		<title> Login </title>
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="vendor/css/normalize.css">	
+		<link rel="stylesheet" href="vendor/css/main.css">	
+ 		<link rel="stylesheet" href="css/style.css">	
+		
+		<!-- Javascript -->
+
+
+    </head>
+		
+		<body>
+			
+			<!-- HEADER: Login Header -->
+			<header>
+				
+			</header>	
+			
+			<!-- BODY: Login Page Body -->
+			<div id = "" class = "">		
+		
+			</div>
+			
+
+	</body>
+</html>
+
+
